15221d848a5a101c7e3cc282fc5dc493efcf7dc0
Vilius Kraujutis <viliusk@gmail.com>
2016-11-27 12:10:32 +0200

    Add img_app_name title as image with custom photo

--- app/src/main/java/lt/andro/understraight/CalibrationActivity.java
+++ app/src/main/java/lt/andro/understraight/CalibrationActivity.java
@@ -2,8 +2,8 @@ package lt.andro.understraight;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class CalibrationActivity extends AppCompatActivity {
 
